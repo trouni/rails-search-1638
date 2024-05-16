77adf1d4eac8cfe2cbd8332fafa7bf135fb8a94f
Trouni Tiet <trouni@gmail.com>
2024-05-16 10:24:10 +0900

    Add PG Search Implementation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,14 +2,18 @@ class MoviesController < ApplicationController
   def index
     @movies = 
       if params[:q].present?
-        sql_query = <<~SQL
-        title @@ :q OR
-        synopsis @@ :q OR
-        directors.first_name @@ :q OR
-        directors.last_name @@ :q
-        SQL
-        # @@ is called the full text search operator
-        Movie.joins(:director).where(sql_query, q: "%#{params[:q]}%")
+        ## PG Search Implementation
+        Movie.search(params[:q])
+
+        ## SQL Implementation
+        # sql_query = <<~SQL
+        # title @@ :q OR
+        # synopsis @@ :q OR
+        # directors.first_name @@ :q OR
+        # directors.last_name @@ :q
+        # SQL
+        # # @@ is called the full text search operator
+        # Movie.joins(:director).where(sql_query, q: "%#{params[:q]}%")
       else
         Movie.all
       end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,17 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  include PgSearch::Model
+  pg_search_scope :search,
+    against: [:title, :synopsis],
+    associated_against: {
+      director: [:first_name, :last_name]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
+  # pg_search_scope :search_only_on_title
+
+  multisearchable against: [:title, :synopsis]
 end
